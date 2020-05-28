14b09ff8b2e0fcbeda5fab756791eaabf910017f
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:20:29 -0600

    add: appoder
    add: appoder

---
+++ Parcial2/Model/APPORDER.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Model
+{
+    class APPORDER
+    {
+        public int idorder { get; set; }
+        public string createdate { get; set; }
+        public int idproduct { get; set; }
+        public string username { get; set; }
+
+        public APPORDER()
+        {
+            idorder = 0;
+            createdate = ""; 
+            idproduct = 0;
+            username = "";
+        }
+    }
+}
