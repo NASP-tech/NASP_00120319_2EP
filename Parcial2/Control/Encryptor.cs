7d34d648cfd8e80d5ca4736a3fce395c640d85b1
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:15:23 -0600

    Add: Encryptor
    Add: Encryptor

---
+++ Parcial2/Control/Encryptor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Control
+{
+    class Encryptor
+    {
+        public static string CreateMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for(int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+
+
+        public static bool CompareMD5(string chain, string nMD5)
+        {
+            string hashOfInput = CreateMD5(chain);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            return (0 == comparer.Compare(hashOfInput, nMD5));
+        }
+    }
+}
