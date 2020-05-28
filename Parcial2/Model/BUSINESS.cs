0293b122e155342cfd83555b547ed749d6a95451
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:20:43 -0600

    Add: BUSINESS
    Add: BUSINESS

---
+++ Parcial2/Model/BUSINESS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Model
+{
+    class BUSINESS
+    {
+        public int idbusiness { get; set; }
+        public string businessname { get; set; }
+        public string description { get; set; }
+
+        public BUSINESS()
+        {
+            idbusiness = 0;
+            businessname = "";
+            description = "";
+        }
+    }
+}
