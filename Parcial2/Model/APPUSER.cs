36b5c8e66afb130fc0eea82e58c9c4584fb744c8
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:20:49 -0600

    Create APPUSER.cs

---
+++ Parcial2/Model/APPUSER.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Model
+{
+    class APPUSER
+    {
+        public string fullname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool administrator { get; set; }
+
+        public APPUSER()
+        {
+            fullname = "";
+            username = "";
+            password = "";
+            administrator = false;
+        }
+
+    }
+}
