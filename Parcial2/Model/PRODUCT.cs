636b9383d6be336b54981cb1c266f62a99a82590
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:21:19 -0600

    ADD: PRODUCT
    ADD: PRODUCT

---
+++ Parcial2/Model/PRODUCT.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Model
+{
+    class PRODUCT
+    {
+        public int idproduct { get; set; }
+        public string businessname { get; set; }
+        public string productname { get; set; }
+
+        public PRODUCT()
+        {
+            idproduct = 0;
+            businessname = "";
+            productname = "";
+        }
+        
+    }
+}
