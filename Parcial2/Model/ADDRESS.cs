97b96519e50117b695c058a444cd428c84043f42
Natalia Solorzano <00120319@uca.edu.sv>
2020-05-28 05:15:44 -0600

    Add: ADDRESS
    Add: ADDRESS

---
+++ Parcial2/Model/ADDRESS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2.Model
+{
+    class ADDRESS
+    {
+        public int idAddress { get; set; }
+        public string username { get; set; }
+        public string address { get; set; }
+
+
+        public ADDRESS()
+        {
+            idAddress = 0;
+            username = "";
+            address = "";
+        }
+       
+    }
+}
